c196447affec24e480079ad84b5cd74af4791177
Justin Holguín <justin.holguin@puppet.com>
2016-04-13 16:30:30 -0700

    create pglogical on test_replica, allow conns

--- master_provision.sh
+++ master_provision.sh
@@ -12,8 +12,8 @@ sudo echo "max_wal_senders = 10        # one per node needed on provider node" >
 sudo echo "listen_addresses = '*'" >> /etc/postgresql/9.5/main/postgresql.conf
 
 echo "updating pg_hba.conf"
+sudo echo "host    replication          rep             192.168.33.10/32 trust" >> /etc/postgresql/9.5/main/pg_hba.conf
 sudo echo "host    test_replication     rep             192.168.33.10/32 trust" >> /etc/postgresql/9.5/main/pg_hba.conf
-# sudo echo "host    test_replication     postgres        192.168.33.10/32 trust" >> /etc/postgresql/9.5/main/pg_hba.conf
 
 echo "restarting postgres"
 sudo service postgresql restart
@@ -21,12 +21,14 @@ sudo service postgresql restart
 # create provider node
 echo "creating the provider node"
 sudo su postgres -c psql <<EOF
+ \c test_replication;
  SELECT pglogical.create_node(node_name := 'provider1', dsn := 'host=192.168.33.2 port=5432 dbname=test_replication');
 EOF
 
 # Add all tables in public schema to the default replication set.
 echo "adding public tables to the default replication set"
 sudo su postgres -c psql <<EOF
+ \c test_replication;
  SELECT pglogical.replication_set_add_all_tables('default', ARRAY['public']);
 EOF
 
--- replica_provision.sh
+++ replica_provision.sh
@@ -4,10 +4,18 @@ IFS=$'\n\t'
 
 
 echo "beginning replica-specific config:"
+sudo echo "listen_addresses = '*'" >> /etc/postgresql/9.5/main/postgresql.conf
+
+echo "updating pg_hba.conf"
+sudo echo "host    replication          rep             192.168.33.10/32 trust" >> /etc/postgresql/9.5/main/pg_hba.conf
+sudo echo "host    test_replication     rep             192.168.33.10/32 trust" >> /etc/postgresql/9.5/main/pg_hba.conf
+
+sudo service postgresql restart
 
 # create the subscriber node
 echo "creating the subscriber node"
 sudo su postgres -c psql <<EOF
+ \c test_replication;
  SELECT pglogical.create_node(node_name := 'subscriber1', dsn := 'host=192.168.33.10 port=5432 dbname=test_replication user=rep');
 EOF
 
@@ -15,8 +23,9 @@ sudo sh -c 'echo "192.168.33.2 master" >> /etc/hosts'
 
 # http://2ndquadrant.com/en-us/resources/pglogical/pglogical-docs/
 
-echo "subscribing (broken right now)"
+echo "subscribing"
 sudo su postgres -c psql <<EOF
+ \c test_replication;
  SELECT pglogical.create_subscription(
      subscription_name := 'subscription1',
      provider_dsn := 'host=master port=5432 dbname=test_replication user=rep'
