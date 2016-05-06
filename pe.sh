37af69d7d184d5f79540b85ed91f97d82a10f76e
Justin Holguin <justin.h.holguin@gmail.com>
2016-05-06 10:46:47 -0700

    update pe version
    note: smoke shows these results:
    * test_replication: no replication (table missing)
    * pe-classifier: no replication (table missing)
    * pe-rbac: table/rows replicating

--- pe.sh
+++ pe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-PE_VERSION="puppet-enterprise-2016.2.0-rc1-92-gb20e673-ubuntu-14.04-amd64"
+PE_VERSION="puppet-enterprise-2016.2.0-rc1-112-ga08a2ea-ubuntu-14.04-amd64"
 
 echo "downloading PE"
 wget --no-verbose http://enterprise.delivery.puppetlabs.net/2016.2/ci-ready/$PE_VERSION.tar
